0f7a25815fa6b5aa281549f575bbe10cd3cc6744
asbelljc <asbelljc@gmail.com>
2022-01-04 01:34:04 -0500

    Added passport JWT strategy to auth.js.

--- auth/auth.js
+++ auth/auth.js
@@ -49,3 +49,22 @@ passport.use(
     }
   )
 );
+
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token'),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
